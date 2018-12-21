4c5132544de04b11243b5330da979ae6a436c8f6
Javed Akhtar <akhtar.ja@gmail.com>
2018-12-21 14:04:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fizzbuzz as a Service
+
+This is a Serverless application for AWS that takes numbers across a range, applies ("fizz buzz")[https://en.wikipedia.org/wiki/Fizz_buzz] to them, and returns the results as an array. This is implemented as a REST API expecting a GET call at the `/fizzbuzz` endpoint.
+
+## Requirements
+This application requires the following:
+1. node and npm (`brew install node`)
+2. Serverless (`npm install --global serverless`)
+3. An AWS local credential file with a `[default]` profile with valid AWS API keys
+
+## Deployment
+1. Clone this repo
+2. `npm install`
+3. `serverless deploy`
+4. The serverless deployment output will show an API URL once complete. Use the API by browsing to `(url)/fizzbuzz?min=[min]&max=[max]`. The API results will be visible as a JSON string in the browser.
+5. To see running logs of the application execution, invoke `serverless logs --function fizzbuzz --tail`
---
+++ handler.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const parseUrlParams = (event) => {
+    const min = parseInt(event.queryStringParameters.min, 10);
+    const max = parseInt(event.queryStringParameters.max, 10);
+
+    return Object.assign(event, { min, max });
+};
+
+const isMultiple = (multiple, multiplier) => multiple % multiplier === 0;
+
+const fizzbuzzify = (event) => {
+    let fizzbuzzed = [];
+    let fizzbuzzHelper = '';
+
+    for (let index = event.min; index <= event.max; index++) {
+        fizzbuzzHelper = '';
+        if (isMultiple(index, 3)) fizzbuzzHelper += 'fizz';
+        if (isMultiple(index, 5)) fizzbuzzHelper += (fizzbuzzHelper.length === 0 ? 'buzz' : ' buzz');
+        if (fizzbuzzHelper.length === 0) fizzbuzzHelper += index;
+        fizzbuzzed.push(fizzbuzzHelper);
+    }
+
+    return Object.assign(event, { fizzbuzzed });
+};
+
+module.exports.fizzbuzz = async (event, context) => {
+    event = fizzbuzzify(parseUrlParams(event));
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            min: event.min,
+            max: event.max,
+            fizzbuzzed: event.fizzbuzzed
+        }),
+    };
+};
